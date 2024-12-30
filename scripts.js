c869622a7d2e9612f6409feb9a40a7ba3a79d813
izek-izzy <izekbanda92@gmail.com>
2024-12-30 15:50:21 +0200

    Update: a create grid function

--- scripts.js
+++ scripts.js
@@ -12,5 +12,24 @@ newGridButton.addEventListener('click', () => {
     createGrid(size);
 });
 
+function createGrid(size) {
+    const container = document.getElementById('container');
+    container.innerHTML = '';
+    const squareSize = 960/size;
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'orange';
+        });
+        container.appendChild(square);
+    }
+}
+
+    
+
+
 
 
